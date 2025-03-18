d3dbabe0ee5a69fd776afcd762c58f16700e75bc
naoalves <145606088+naomialves@users.noreply.github.com>
2025-03-18 10:20:57 -0300

    Update main.js

--- naomi pasta agrinho/main.js
+++ naomi pasta agrinho/main.js
@@ -1,8 +1,24 @@
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2030-01-01T00:00:00");
 
-const tempo = tempoObjetivo1 ;
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -11,31 +27,10 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-  
+
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    if (tempoFinal > 0) {
-      return [dias, horas, minutos, segundos];
-    } else {
-      return [0, 0, 0, 0];
-    }
-  }
 
-  function atualizaCronometro() {
-    document.getElementById("dias0").textContent = calculaTempo(tempo)[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempo)[1];
-    document.getElementById("min0").textContent = calculaTempo(tempo)[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempo)[3];
-  
-    for (let i = 0; i < contadores.length; i++) {
-      // contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-  }
-
-  function comecaCronometro() {
-    atualizaCronometro();
-    setInterval(atualizaCronometro, 1000);
-  }
-  
-  comecaCronometro();
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
